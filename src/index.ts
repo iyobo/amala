3a8367e91219b28163e7c34779a6e68c8944fb13
iyobo <phil.eki@gmail.com>
2021-03-25 09:28:39 -0500

    better types

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,8 @@ export interface KoaControllerOptions {
   versions?: Array<number | string> | { [key: string]: string | boolean };
   disableVersioning?: boolean;
   router?: any;
-  flow?: Array<Function>;
-  errorHandler?: Function;
+  flow?: Array<(ctx, next) => void>;
+  errorHandler?: (err, ctx) => void;
 
   // if true, will attach generated routes to the koa app. Don't set to true if you need to use app.use(...)
   attachRoutes?: boolean;
